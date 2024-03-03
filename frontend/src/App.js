3faea5ade2ec96f4641e3b6f338aa2636693b1dc
SimonG11 <simon.gottschalk@hotmail.com>
2024-03-03 14:14:04 +0100

    dd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-function App() {
+const Home = () => <div>Home Page <Link to="/about">Go to About</Link></div>;
+const About = () => <div>About Page <Link to="/about/team">Our Team</Link></div>;
+const Team = () => <div>Team Page</div>;
+const NoMatch = () => <div>404 Page Not Found</div>;
+
+const App = () => {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route path="/about/team" component={Team} />
+        <Route component={NoMatch} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
